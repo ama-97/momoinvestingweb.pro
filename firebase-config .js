5ac35fd8e71c82f52d60e7763acbed3c634e499d
ama-97 <mtraderatike@gmail.com>
2025-07-17 11:19:17 +0000

    Update firebase-config .js

--- firebase-config .js
+++ firebase-config .js
@@ -1,22 +1,16 @@
-// firebase-config.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
+<script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-auth.js"></script>
+<script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-database.js"></script>
+<script>
+  const firebaseConfig = {
+    apiKey: "AIzaSyA-l8gt0GX8usilrR5afpiAHIruSQhrRR4",
+    authDomain: "momoinvesting-e85dc.firebaseapp.com",
+    projectId: "momoinvesting-e85dc",
+    storageBucket: "momoinvesting-e85dc.appspot.com",
+    messagingSenderId: "125503752102",
+    appId: "1:125503752102:web:a5ee7dd0155e85fbe93167",
+    databaseURL: "https://momoinvesting-e85dc-default-rtdb.firebaseio.com"
+  };
 
-// Configuration de ton projet Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyA-l8gt0GX8usilrR5afpiAHIruSQhrRR4",
-  authDomain: "momoinvesting-e85dc.firebaseapp.com",
-  projectId: "momoinvesting-e85dc",
-  storageBucket: "momoinvesting-e85dc.appspot.com",
-  messagingSenderId: "125503752102",
-  appId: "1:125503752102:web:a5ee7dd0155e85fbe93167",
-  databaseURL: "https://momoinvesting-e85dc-default-rtdb.firebaseio.com" // ← ajoute cette ligne si tu veux utiliser la Realtime Database
-};
-
-// Initialisation de Firebase
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const database = getDatabase(app);
-
-export { app, auth, database };
+  firebase.initializeApp(firebaseConfig);
+</script>
