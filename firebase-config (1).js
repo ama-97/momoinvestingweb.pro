c56ca746e2bfb4fde84f26634ddfb8fef3405185
ama-97 <mtraderatike@gmail.com>
2025-07-17 07:08:58 +0000

    Add files via upload

---
+++ firebase-config (1).js
@@ -0,0 +1,13 @@
+
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyA-l8gt0GX8usilrR5afpiAHIruSQhrRR4",
+  authDomain: "momoinvesting-e85dc.firebaseapp.com",
+  projectId: "momoinvesting-e85dc",
+  storageBucket: "momoinvesting-e85dc.appspot.com",
+  messagingSenderId: "125503752102",
+  appId: "1:125503752102:web:a5ee7dd0155e85fbe93167"
+};
+
+// Initialiser Firebase
+firebase.initializeApp(firebaseConfig);
