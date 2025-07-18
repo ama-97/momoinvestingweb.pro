88ed2139fa7f340993bf58ef795fc367b67d631c
ama-97 <mtraderatike@gmail.com>
2025-07-18 20:21:13 +0000

    Update firebase-config .js

--- firebase-config .js
+++ firebase-config .js
@@ -1,11 +1,1 @@
-// firebase-config.js
-const firebaseConfig = {
-  apiKey: "AIzaSyA-l8gtlOGX8usilR5afpiAHIRuSQhrRR4",
-  authDomain: "momoinvesting-e85dc.firebaseapp.com",
-  projectId: "momoinvesting-e85dc",
-  storageBucket: "momoinvesting-e85dc.appspot.com",
-  messagingSenderId: "125503752102",
-  appId: "1:125503752102:web:a5ee7dd0155e85fbe93167",
-  databaseURL: "https://momoinvesting-e85dc-default-rtdb.firebaseio.com"
-};
-firebase.initializeApp(firebaseConfig);
+
