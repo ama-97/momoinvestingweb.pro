abff7bbfe5fd615be135b8dc0c2c7fe0cf40c4f4
ama-97 <mtraderatike@gmail.com>
2025-07-18 09:01:37 +0000

    Update firebase-config .js

--- firebase-config .js
+++ firebase-config .js
@@ -1,15 +1,11 @@
-<script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js"></script>
-<script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-auth.js"></script>
-<script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-database.js"></script>
-<script>
-  const firebaseConfig = {
-    apiKey: "AIzaSyA-l8gt0GX8usilrR5afpiAHIruSQhrRR4",
-    authDomain: "momoinvesting-e85dc.firebaseapp.com",
-    projectId: "momoinvesting-e85dc",
-    storageBucket: "momoinvesting-e85dc.appspot.com",
-    messagingSenderId: "125503752102",
-    appId: "1:125503752102:web:a5ee7dd0155e85fbe93167",
-    databaseURL: "https://momoinvesting-e85dc-default-rtdb.firebaseio.com"
-  };
-firebase.initializeapp(firebaseconfig);
-</script>
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyA-l8gtlOGX8usilR5afpiAHIRuSQhrRR4",
+  authDomain: "momoinvesting-e85dc.firebaseapp.com",
+  projectId: "momoinvesting-e85dc",
+  storageBucket: "momoinvesting-e85dc.appspot.com",
+  messagingSenderId: "125503752102",
+  appId: "1:125503752102:web:a5ee7dd0155e85fbe93167",
+  databaseURL: "https://momoinvesting-e85dc-default-rtdb.firebaseio.com"
+};
+firebase.initializeApp(firebaseConfig);
